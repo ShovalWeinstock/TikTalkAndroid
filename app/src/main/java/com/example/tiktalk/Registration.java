da525c3daa1b6d5bbd5c5d8dae796fe03e22090e
shoval <shoval.weinstock@live.biu.ac.il>
2022-06-04 15:27:59 +0300

    working on registration validation

--- app/src/main/java/com/example/tiktalk/Registration.java
+++ app/src/main/java/com/example/tiktalk/Registration.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.provider.MediaStore;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 
 
@@ -11,24 +13,81 @@ import android.os.Bundle;
 
 public class Registration extends AppCompatActivity {
 
+    EditText et_username, et_nickname, et_password, et_confirmation;
+    Button register_btn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
 
-        ImageView imageView = findViewById(R.id.imageView);
 
-        imageView.setOnClickListener(v -> {
-            Intent openGalleryIntent = new Intent(Intent.ACTION_PICK,
-                                                  MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-            startActivityForResult(openGalleryIntent, 1000);
+        et_username = findViewById(R.id.registrationUsername);
+        et_nickname = findViewById(R.id.registrationNickname);
+        et_password = findViewById(R.id.registrationPassword);
+        et_confirmation = findViewById(R.id.registrationConfirmPassword);
 
-            @Override
-            protected void onActivityResult(int requestCode, int resultCode, @android.annotation.Nullable Intent data) {
+        register_btn = findViewById(R.id.register_btn);
 
-            }
+        register_btn.setOnClickListener(v -> {
+            String username = et_username.getText().toString();
+            String nickname = et_nickname.getText().toString();
+            String password = et_password.getText().toString();
+            String confirmation = et_confirmation.getText().toString();
+
+            Boolean valid = validate(username, password, confirmation);
+
+//            if(valid) {
+//                // register the user
+//            }
 
         });
 
+       // ImageView imageView = findViewById(R.id.imageView);
+        //imageView.setOnClickListener(v -> {
+          //  Intent openGalleryIntent = new Intent(Intent.ACTION_PICK,
+            //                                      MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+            //startActivityForResult(openGalleryIntent, 1000);
+
+            //@Override
+            //protected void onActivityResult(int requestCode, int resultCode, @androidx.annotation.Nullable Intent data) {
+
+        //});
+    }
+
+    private Boolean validate(String username, String password, String confirmation) {
+        // validate username
+        if(username.length() == 0) {
+            et_username.requestFocus();
+            et_username.setError("Username is required");
+            return false;
+        }
+        else if(username.equals("exists")) { //todo check if username exists
+            et_username.requestFocus();
+            et_username.setError("Username already exists");
+            return false;
+        }
+        // validate password
+        if(password.length() == 0) {
+            et_password.requestFocus();
+            et_password.setError("Password is required");
+            return false;
+        }
+        else if(!password.equals("regex")) { //todo check if password is valid
+            et_password.requestFocus();
+            et_password.setError("Invalid password");
+        }
+        // validate conformation
+        if(confirmation.length() == 0) {
+            et_confirmation.requestFocus();
+            et_confirmation.setError("Password confirmation is required");
+            return false;
+        }
+        else if(!confirmation.equals(password)) {
+            et_confirmation.requestFocus();
+            et_confirmation.setError("Passwords don't match");
+            return false;
+        }
+        return true;
     }
 }
