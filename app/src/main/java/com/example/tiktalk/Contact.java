420fc2965580f2214cbabaa8aa34cffedcd414e0
itamar-st <81354824+itamar-st@users.noreply.github.com>
2022-06-05 17:03:13 +0300

    contacts activity with add contact btn. room infrastructure set

---
+++ app/src/main/java/com/example/tiktalk/Contact.java
@@ -0,0 +1,55 @@
+package com.example.tiktalk;
+
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity
+public class Contact {
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+    private String name;
+    private String last;
+    private String lastDate;
+
+    public Contact(int id, String name, String last, String lastDate) {
+        this.id = id;
+        this.name = name;
+        this.last = last;
+        this.lastDate = lastDate;
+    }
+
+    public Contact() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLast() {
+        return last;
+    }
+
+    public void setLast(String last) {
+        this.last = last;
+    }
+
+    public String getLastDate() {
+        return lastDate;
+    }
+
+    public void setLastDate(String lastDate) {
+        this.lastDate = lastDate;
+    }
+}
