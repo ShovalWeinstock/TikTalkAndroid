1e5b2f7bb69afdcf904d25bb962fe95cc5e95597
itamar-st <81354824+itamar-st@users.noreply.github.com>
2022-06-07 14:51:07 +0300

    add, delete and remove contact with room

---
+++ app/src/main/java/com/example/tiktalk/UpdateContactActivity.java
@@ -0,0 +1,46 @@
+package com.example.tiktalk;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.room.Room;
+
+public class UpdateContactActivity extends AppCompatActivity {
+
+    private AppDB db;
+    private ContactDao contactDao;
+    EditText et_nickname;
+    Button updateContactBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_contact);
+        //reference to Dao
+        db = Room.databaseBuilder(getApplicationContext(), AppDB.class, "ContactDB")
+                .allowMainThreadQueries()
+                .fallbackToDestructiveMigration()
+                .build();
+        contactDao = db.contactDao();
+
+        // nickname bar with the current nickname
+        et_nickname = findViewById(R.id.et_updateContactNickname);
+        String id = getIntent().getExtras().getString("id");
+        Contact contact = contactDao.get(id);
+        et_nickname.setText(contact.getName());
+
+        //save new name into the db
+        updateContactBtn = findViewById(R.id.update_contact_btn);
+        updateContactBtn.setOnClickListener(v -> {
+            // get username and password, and validate them
+                contact.setName(et_nickname.getText().toString());
+                contactDao.update(contact);
+            finish();
+        });
+
+
+
+    }
+}
